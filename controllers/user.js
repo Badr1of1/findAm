642ad34927e5de00b4f28c9575984cac9d6df7d7
Badr Yakub <badrakblink@gmail.com>
2024-04-03 06:14:42 +0000

    refactored signup logic

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,29 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const signUp = async (req, res) => {
   try {
-    const user = req.body;
-    const newUser = await User.create(user);
-    res.status(201).json(newUser);
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+     return res.status(400).json({msg:"Please fill out the necessary fields"})
+    }
+
+    const existingUsername = await User.findOne({ username });
+    const existingEmail = await User.findOne({ email });
+
+    if (existingUsername) {
+      return res.status(400).json({ msg: "Username already exists" });
+    }
+    if (existingEmail) {
+      return res.status(400).json({ msg: "Email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ username, email, password: hashedPassword });
+    res.status(201).json({ msg: "User created successfully", newUser });
   } catch (error) {
-    res.status(500).json({ error: "Failed to sign up. Please try again" });
+    console.error(error);
+    res.status(500).json({ msg: "Internal server error" });
   }
 };
 
