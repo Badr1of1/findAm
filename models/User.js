d5e587514890dc4eef66a53241342154a22d36d1
Badr Yakub <badrakblink@gmail.com>
2024-04-05 00:13:07 +0000

    signUp schema refactor

--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: {
+  username: {
     type: String,
-    required: true,
+    required: [true, "Please provide a user name"],
   },
 
   email: {
