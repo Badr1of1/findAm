189d8b55c197b3ccde5f1b26fd27ea296717ad90
Badr Yakub <badrakblink@gmail.com>
2024-05-21 13:09:46 +0000

    refactored code and removed redundant code

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-// const {
-//     authItems,
-//     authSingleItem,
-//     authDelitem,
-//     authUpdate,
-//   } = require("../controllers/auth/items");
-// const authenticateUser = require("../middlewares/auth");
-
 const { login, signUp, signOut } = require("../controllers/user");
 
 router.route("/signup").post(signUp);
 router.route("/login").post(login);
 router.route("/login").post(signOut);
 
-// router.route("/user/items").get(authenticateUser, authItems);
-
-// router.route("/user/items:id").get(authenticateUser, authSingleItem).delete(authenticateUser, authDelitem).patch(authenticateUser, authUpdate);
 
 module.exports = router;
