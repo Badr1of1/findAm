e84b1daf1af0fb88a88d7cba11c0d9b01392f7b1
Badr Yakub <badrakblink@gmail.com>
2024-07-23 14:32:59 +0000

    away with multers3 v1

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(notFoundMw);
 port = process.env.PORT || 5000;
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_LOCAL_URI);
+    await connectDB(process.env.MONGO_URI);
     app.listen({ port }, () => {
       console.log(`Server is listening on port ${port}...`);
     });
